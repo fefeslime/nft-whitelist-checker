36087d8a701d8ed12a7a80abd42872cfc51f784d
fefeslime <mehdi@slime.marketing>
2024-11-25 17:07:26 +0300

    Update checkWhitelist.js

--- functions/checkWhitelist.js
+++ functions/checkWhitelist.js
@@ -1,35 +1,41 @@
 // functions/checkWhitelist.js
 
 const whitelist = require('./whitelist.json');
-  
-  exports.handler = async (event, context) => {
-    const { wallet } = event.queryStringParameters;
-  
-    if (!wallet) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ success: false, message: "No wallet parameter provided." }),
-      };
-    }
-  
-    // Basic Wallet Address Validation
-    const walletRegex = /^0x[a-fA-F0-9]{40}$/;
-    if (!walletRegex.test(wallet)) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ success: false, message: "Invalid wallet address format." }),
-      };
-    }
-  
-    // Normalize wallet address for comparison
-    const normalizedWallet = wallet.toLowerCase();
-  
-    // Check if the wallet is in the whitelist
-    const isWhitelisted = whitelist.some(addr => addr.toLowerCase() === normalizedWallet);
-  
+
+exports.handler = async (event, context) => {
+  const { wallet } = event.queryStringParameters;
+
+  if (!wallet) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ success: false, message: "No wallet parameter provided." }),
+    };
+  }
+
+  // Basic Wallet Address Validation
+  const walletRegex = /^0x[a-fA-F0-9]{40}$/;
+  if (!walletRegex.test(wallet)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ success: false, message: "Invalid wallet address format." }),
+    };
+  }
+
+  // Normalize wallet address for comparison
+  const normalizedWallet = wallet.toLowerCase();
+
+  // Find the wallet in the whitelist
+  const found = whitelist.find(addr => addr.address.toLowerCase() === normalizedWallet);
+
+  if (found) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ success: true, whitelisted: isWhitelisted }),
+      body: JSON.stringify({ success: true, whitelisted: true, type: found.type }),
     };
-  };
-  
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, whitelisted: false }),
+    };
+  }
+};
